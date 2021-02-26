0bc5cb9d904b574f10513277f1e4d4b85dec0179
Divya-sen-au6 <58457930+Divya-sen-au6@users.noreply.github.com>
2021-02-26 17:45:59 +0530

    added KeyboardDatePicker

--- src/modules/app/App.tsx
+++ src/modules/app/App.tsx
@@ -1,85 +1,86 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { ThemeProvider } from 'styled-components'
 import { theme, GlobalStyle } from 'shared/styles/theme'
-import { authStart, setSocketConnection } from 'logic/actions/user.actions'
+import { authStart } from 'logic/actions/user.actions'
 import Routes from './components/routes/Routes'
-import io from 'socket.io-client'
-import { apiBaseUrl } from 'services/global-constant'
-import { SocketContext, socket } from 'context/socket'
+
 const App = (props: any) => {
-  const { authStart, loggedIn } = props
-  const [initDone, setInitDone] = useState(false)
-  
+  const { authStart } = props
+
   useEffect(() => {
-    if (apiBaseUrl !== 'https://13.82.217.131:3001') {
-    }
     authStart()
   }, [authStart])
 
-  useEffect(() => {
-    if (loggedIn) {
-      const socket = io(`wss://${apiBaseUrl.split('//')[1]}`)
-      socket.on('connection', () => {
-        if (socket.connected) {
-          socket.emit('user', {
-            token: localStorage.getItem('token'),
-          })
-          setSocketConnection(socket)
-        } else {
-          socket.on('exception', function () {})
-        }
-      })
-    }
-  }, [loggedIn, setSocketConnection])
-
   return (
-    <>
-      {/* <SocketContext.Provider value={{ initDone }}> */}
-      {initDone && (
-        <ThemeProvider theme={theme}>
-          <GlobalStyle />
-          <Routes />
-        </ThemeProvider>
-      )}
-
-      {/* </SocketContext.Provider> */}
-    </>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Routes />
+    </ThemeProvider>
   )
 }
 
 const mapStateToProps = (state: any) => ({
   loggedIn: state.user.loggedIn,
-  socketApi: state.user.socketApi,
 })
-export default connect(mapStateToProps, { authStart, setSocketConnection })(App)
+export default connect(mapStateToProps, { authStart })(App)
 
- // //socket.emit allows you to emit custom events on the server and client
- // //socket.send sends messages which are received with the 'message' event
-
-// import React, { useEffect } from 'react'
+// import React, { useEffect, useState } from 'react'
 // import { connect } from 'react-redux'
 // import { ThemeProvider } from 'styled-components'
 // import { theme, GlobalStyle } from 'shared/styles/theme'
-// import { authStart } from 'logic/actions/user.actions'
+// import { authStart, setSocketConnection } from 'logic/actions/user.actions'
 // import Routes from './components/routes/Routes'
-
+// import io from 'socket.io-client'
+// import { apiBaseUrl } from 'services/global-constant'
+// import { SocketContext, socket } from 'context/socket'
 // const App = (props: any) => {
-//   const { authStart } = props
-
+//   const { authStart, loggedIn } = props
+//   const [initDone, setInitDone] = useState(false)
+  
 //   useEffect(() => {
+//     if (apiBaseUrl !== 'https://13.82.217.131:3001') {
+//     }
 //     authStart()
 //   }, [authStart])
 
+//   useEffect(() => {
+//     if (loggedIn) {
+//       const socket = io(`wss://${apiBaseUrl.split('//')[1]}`)
+//       socket.on('connection', () => {
+//         if (socket.connected) {
+//           socket.emit('user', {
+//             token: localStorage.getItem('token'),
+//           })
+//           setSocketConnection(socket)
+//         } else {
+//           socket.on('exception', function () {})
+//         }
+//       })
+//     }
+//   }, [loggedIn, setSocketConnection])
+
 //   return (
-//     <ThemeProvider theme={theme}>
-//       <GlobalStyle />
-//       <Routes />
-//     </ThemeProvider>
+//     <>
+//       {/* <SocketContext.Provider value={{ initDone }}> */}
+//       {initDone && (
+//         <ThemeProvider theme={theme}>
+//           <GlobalStyle />
+//           <Routes />
+//         </ThemeProvider>
+//       )}
+
+//       {/* </SocketContext.Provider> */}
+//     </>
 //   )
 // }
 
 // const mapStateToProps = (state: any) => ({
 //   loggedIn: state.user.loggedIn,
+//   socketApi: state.user.socketApi,
 // })
-// export default connect(mapStateToProps, { authStart })(App)
+// export default connect(mapStateToProps, { authStart, setSocketConnection })(App)
+
+ // //socket.emit allows you to emit custom events on the server and client
+ // //socket.send sends messages which are received with the 'message' event
+
--- src/shared/components/form-date-picker/FormDatePicker.tsx
+++ src/shared/components/form-date-picker/FormDatePicker.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Field } from 'formik'
 import TextField from '@material-ui/core/TextField'
 import { createStyles, makeStyles } from '@material-ui/core/styles'
 import { colors } from 'shared/styles/theme'
+import DateFnsUtils from '@date-io/date-fns'; 
+import { KeyboardDatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
 
 const textFieldStyle = makeStyles(() =>
   createStyles({
@@ -10,6 +12,7 @@ const textFieldStyle = makeStyles(() =>
       marginBottom: '20px',
       lineHeight: '26px',
       color: '#1E3444',
+      width: '100%',
       backgroundColor: colors.white,
     },
   })
@@ -27,25 +30,35 @@ const FormDatePicker = (props: Props) => {
   return (
     <Field name={name}>
       {({ field, form }: any) => {
-        const handleChange = (event: any) => {
-          const fieldVal: any = event.target.value
-          form.setFieldValue(field.name, fieldVal)
+        // const handleChange = (event: any) => {
+        //   const fieldVal: any = event.target.value
+        //   form.setFieldValue(field.name, fieldVal)
+        // }
+        const handleChange = (date:Date|null) => {
+          const fieldVal:any =date
+          form.setFieldValue(field.name,fieldVal)
         }
+
         return (
-          <TextField
-            variant="outlined"
-            fullWidth
-            id={name}
-            label={label}
-            name={name}
-            value={field.value}
-            autoComplete={name}
-            autoFocus
-            onChange={handleChange}
-            type={!!type ? type : 'date'} 
-            className={classes.root}
-            InputLabelProps={{ shrink: true }}
-          />
+
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <KeyboardDatePicker
+              disableToolbar
+              inputVariant="outlined"
+              className={classes.root}
+              variant="inline"
+              format="dd/MM/yyyy"
+              margin="normal"
+              id="date-picker-inline"
+              label="Year Built"
+              value={field.value?field.value:null}
+              onChange={() => handleChange}
+              KeyboardButtonProps={{
+                'aria-label': 'change date',
+              }}
+              placeholder="dd/mm/yyyy"
+            />
+          </MuiPickersUtilsProvider>
         )
       }}
     </Field>

7e982151647e94e74117041134379534aee6ee75
Aman Goyal <ag@rapidinnovation.dev>
2021-03-10 12:39:20 +0530

    move account change handler to TopPanel.jsx

--- src/modules/app/components/navbar/top-panel/TopPanel.tsx
+++ src/modules/app/components/navbar/top-panel/TopPanel.tsx
@@ -3,8 +3,10 @@ import Button from '@material-ui/core/Button'
 import Web3 from 'web3'
 
 import { useStyles, TopPanelCont } from './style'
+import { connect } from 'react-redux'
+import { logout } from '../../../../../logic/actions/user.actions'
 
-const TopPanel = () => {
+const TopPanel = (props: any) => {
   const [account, setAccount] = useState(false)
   const classes = useStyles()
   const web3: Web3 = new Web3(window.ethereum)
@@ -21,6 +23,19 @@ const TopPanel = () => {
     }
     getAccount()
   }, 1000)
+
+  // Event handler to handle metamask account change
+  if (window.ethereum) {
+    window.ethereum.on('accountsChanged', function () {
+      props.logout()
+    })
+  }
+
   return <TopPanelCont>{!account && <Button className={classes.buttonStyle}>Connect Wallet</Button>}</TopPanelCont>
 }
-export default TopPanel
+
+const mapStateToProps = (state: any) => ({
+  loggedIn: state.user.loggedIn,
+})
+
+export default connect(mapStateToProps, { logout })(TopPanel)

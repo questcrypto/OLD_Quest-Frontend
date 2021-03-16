a0d3f8660114bb662542b1f1ba9bce974719a735
Aman Goyal <ag@rapidinnovation.dev>
2021-03-16 13:08:00 +0530

    fix issues in metamask account change

--- src/modules/app/components/navbar/top-panel/TopPanel.tsx
+++ src/modules/app/components/navbar/top-panel/TopPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Button from '@material-ui/core/Button'
 import Web3 from 'web3'
 
@@ -7,22 +7,48 @@ import { connect } from 'react-redux'
 import { logout } from '../../../../../logic/actions/user.actions'
 
 const TopPanel = (props: any) => {
+  console.log(props)
   const [account, setAccount] = useState(false)
   const classes = useStyles()
   const web3: Web3 = new Web3(window.ethereum)
-  setInterval(function () {
-    async function getAccount() {
-      const coinbase = await web3?.eth.getCoinbase()
-      if (!coinbase) {
-        setAccount(false)
+
+  // setInterval(function () {
+  //   async function getAccount() {
+  //     const coinbase = await web3?.eth.getCoinbase()
+  //     console.log('auth', coinbase)
+  //     if (!coinbase) {
+  //       setAccount(false)
+  //       return
+  //     }
+  //     const publicaddress = coinbase.toLowerCase()
+
+  //     if (publicaddress) setAccount(true)
+  //   }
+  //   getAccount()
+  // }, 1000)
+
+  const checkWalletAccount = async () => {
+    const coinbase = await web3?.eth.getCoinbase()
+    if (props.userInfo && coinbase) {
+      if (props.userInfo.publicaddress !== coinbase) {
+        console.log(props.userInfo.publicaddress, coinbase)
+        props.logout()
         return
       }
-      const publicaddress = coinbase.toLowerCase()
-
-      if (publicaddress) setAccount(true)
     }
-    getAccount()
-  }, 1000)
+
+    if (!coinbase) {
+      setAccount(false)
+      return
+    }
+    const publicaddress = coinbase.toLowerCase()
+
+    if (publicaddress) setAccount(true)
+  }
+
+  useEffect(() => {
+    checkWalletAccount()
+  }, [])
 
   // Event handler to handle metamask account change
   if (window.ethereum) {
@@ -36,6 +62,7 @@ const TopPanel = (props: any) => {
 
 const mapStateToProps = (state: any) => ({
   loggedIn: state.user.loggedIn,
+  userInfo: state.user.userInfo,
 })
 
 export default connect(mapStateToProps, { logout })(TopPanel)

7453294bc4cac9267e77a12c4e953def8373901a
YATHENDRA <yathendra1995@gmail.com>
2021-08-12 17:45:47 +0530

    Few changes in Google Analytics checking

--- src/modules/app/components/history.ts
+++ src/modules/app/components/history.ts
@@ -31,24 +31,23 @@ const ga4react = new GA4React('G-9WWD66Z78C');
 const history = createBrowserHistory();
 
 ga4react.initialize().then((ga4) => {
-  console.log('ga4', ga4)
-  history.listen((location) => {
-    const loc = location.pathname;
-    const query = location.search;
-    let queryStr = query.replace("?", "");
-    const str = loc + '?' + queryStr;
-    let strNew = str.replace("=", "");
-    // console.log(":rocket: ~ file: history.ts ~ line 14 ~ history.listen ~ strNew", queryStr)
-    ga4.pageview(strNew);
-  });
-  // workaround for initial visit
-  if (
-    window.performance &&
-    performance.navigation.type === performance.navigation.TYPE_NAVIGATE
-  ) {
-    ga4.pageview("/");
-  }
-  // ga4.pageview('path')
+  // history.listen((location) => {
+  //   const loc = location.pathname;
+  //   const query = location.search;
+  //   let queryStr = query.replace("?", "");
+  //   const str = loc + '?' + queryStr;
+  //   let strNew = str.replace("=", "");
+  //   ga4.pageview(strNew);
+  // });
+  // // workaround for initial visit
+  // if (
+  //   window.performance &&
+  //   performance.navigation.type === performance.navigation.TYPE_NAVIGATE
+  // ) {
+  //   ga4.pageview("/");
+  // }
+
+  ga4.pageview(window.location.pathname + window.location.search)
 }, (err) => {
   console.error(err)
 })

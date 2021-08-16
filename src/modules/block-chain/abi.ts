2fdbe7f839128ac4c26204b2377af44baa235247
YATHENDRA <yathendra1995@gmail.com>
2021-08-16 18:28:40 +0530

    Updated Reward address

--- src/modules/block-chain/abi.ts
+++ src/modules/block-chain/abi.ts
@@ -20,7 +20,7 @@ export const LPTokenAddress = '0x6950Fcb879b7907609ed6a5E5f0f60Fbc6Ecb47C'
 export const stratAddress1 = '0xA894BDF433e02D7c46122f2c2fc097b66f7e8269'
 export const stratAddress2 = '0x2B8B86DF468A8D8575eE35f3A5a117C88A9FE4D6'
 export const stratAddress3 = '0x00ecA350a5fD9D5f9c87b0B34d0008cE691d6cE0'
-export const rewardsAddress = '0xf0405Af6C8Dbcc1978Cd77ab500c7AdBf47Cb1c5'
+export const rewardsAddress = '0xEf7De2Cc37e107d64e8AD7B6Ffba95710c110B1B'
 export const questAddress = '0x9ba8158472404a9e49Fd7395A47D826C45afC9C0'
 
 export const slcAbi: any = [

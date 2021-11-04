6a0fe790bc07198fbbc03d58e06f163c2305484e
Akhil Bharti <akhil.bh96@gmail.com>
2021-11-04 17:29:56 +0530

    added factory abi

---
+++ src/modules/property-form/FACTORY_ABI.ts
@@ -0,0 +1,61 @@
+export const ERC1155FACTORYABI:any = [
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_baseURI",
+				"type": "string"
+			},
+			{
+				"internalType": "address",
+				"name": "_treasuryAdmin",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_managingCompany",
+				"type": "address"
+			},
+			{
+				"internalType": "bytes",
+				"name": "_rightToManagementURI",
+				"type": "bytes"
+			},
+			{
+				"internalType": "bytes",
+				"name": "_rightToEquityURI",
+				"type": "bytes"
+			},
+			{
+				"internalType": "bytes",
+				"name": "_rightToControlURI",
+				"type": "bytes"
+			},
+			{
+				"internalType": "bytes",
+				"name": "_rightToResidencyURI",
+				"type": "bytes"
+			},
+			{
+				"internalType": "bytes",
+				"name": "_rightToSubsurfaceURI",
+				"type": "bytes"
+			}
+		],
+		"name": "deployQuestCryptoAsset",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]
